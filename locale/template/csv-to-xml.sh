5367e24dce4edeb6020b24447a46b916241ba82a
Smile4ever <computergeoffrey@zoho.com>
2015-06-20 20:50:25 +0200

    Script to generate xml from LibreOffice csv Add translator to template

---
+++ locale/template/csv-to-xml.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#Script to convert LibreOffice csv in the parent of the current directory to xml
+#empty.xml is the LibreOffice template to quickly create the input csv
+#
+#Input
+#
+#<dialogtitle_normal>,Text,</dialogtitle_normal>
+#<dialogtitle_notfound>,Text,</dialogtitle_notfound>
+#<dialogtitle_lyricsof>,Text,</dialogtitle_lyricsof>
+#
+#Output
+#
+#<dialogtitle_normal>Text</dialogtitle_normal>
+#<dialogtitle_notfound>Text</dialogtitle_notfound>
+#<dialogtitle_lyricsof>Text</dialogtitle_lyricsof>
+
+cd ..
+for filename in `ls *.csv`
+do
+	sed -i 's/>,/>/' $filename
+	sed -i 's/,</</' $filename
+	sed -i 's/,,//' $filename
+	mv "$filename" "`basename $filename .csv`.xml"
+done
